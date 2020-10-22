e1d4d19f894c1dc1be6e8f23162c2b16c49009ca
张吉南 <73272043+zalamp@users.noreply.github.com>
2020-10-22 15:18:52 +0800

    Create mis.cpp

---
+++ mis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//名字类
+class Name {
+    public:
+        void GetData ( );
+        void PrintData ( );
+    private:
+        string hero;
+        string goods;
+        string nenmy;
+};
+
+int main ( ) {
+    return 0;
+}
