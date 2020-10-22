f5e7fae3c6abd43b0ed8f90821641acfa31057e4
张吉南 <73272043+zalamp@users.noreply.github.com>
2020-10-22 16:13:36 +0800

    Update mis.cpp

--- mis.cpp
+++ mis.cpp
@@ -4,14 +4,31 @@ using namespace std;
 //名字类
 class Name {
     public:
-        void GetData ( );
+        void GetData ( string );
         void PrintData ( );
     private:
         string hero;
         string goods;
         string nenmy;
 };
-
+//
+void Name::GetData ( string name ) {
+    hero = name;
+    goods = name;
+    nenmy = name;
+}
+//
+void Name::PrintData ( ) {
+    cout << hero << endl << goods << endl << nenmy << endl;
+}
+//
 int main ( ) {
+    Name name;
+    string text;
+
+    cin >> text;
+    name.GetData ( text );
+    name.PrintData ( );
+
     return 0;
 }
